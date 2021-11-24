a9188c6d6e2622be40081ff57c16f878b4dc8e27
Guilherme Borges <Guilherme.Borges@btgpactual.com>
2021-11-24 14:51:28 -0300

    login.spec.js tests the login with email and password, after signup test

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,6 @@
+it('successfully logs in', () => {
+  cy.intercept('GET', '**/notes').as('getNotes')
+  cy.login()
+  cy.wait('@getNotes')
+  cy.contains('h1', 'Your Notes').should('be.visible')
+})
