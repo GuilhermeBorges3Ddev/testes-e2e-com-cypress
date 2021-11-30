55e896856b92a2a4609867343fba850b61731c35
Guilherme Borges <Guilherme.Borges@btgpactual.com>
2021-11-30 12:51:15 -0300

    cy.session() used in login to do not redo the process in other screens of tests

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,8 +1,8 @@
 it('successfully logs in', () => {
   cy.intercept('GET', '**/notes').as('getNotes')
   cy.login(
-    Cypress.env('USER_EMAIL'), 
-    Cypress.env('USER_PASSWORD'), 
+    Cypress.env('USER_EMAIL'),
+    Cypress.env('USER_PASSWORD'),
     {cacheSession: false}
   )
   cy.wait(15000)
