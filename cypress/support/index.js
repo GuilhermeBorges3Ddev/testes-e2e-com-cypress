7a5e26a89acd523c07ce72e237d52aaa4744e10a
GuilhermeBorges3Ddev <gsilvaborges@gmail.com>
2021-12-19 15:23:20 -0300

    Error comment removed

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -2,7 +2,3 @@ import './commands'
 import 'cypress-mailosaur'
 import 'cypress-file-upload'
 import 'cypress-iframe'
-
-/**
-    npx cypress run --record --key f2eda077-1526-4cce-a68d-552009175dc5
-*/
